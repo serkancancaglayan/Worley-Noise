12044079188c6b539bd7f3977f19292f3e63b52f
Serkan Can Caglayan <49690023+serkancancaglayan@users.noreply.github.com>
2020-10-10 02:37:31 +0300

    Add files via upload

---
+++ funcs.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SDL.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+const int WindowHeight = 400;
+const int WindowWidth = 600;
+const int number_of_random_points = 48;
+
+float distance(int x1, int y1, int x2, int y2) {
+	return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+int worley_noise(vector<SDL_Point>& v, int x, int y, int n) {
+	assert(n <= number_of_random_points);
+	vector<float> dists;
+	for (SDL_Point& p : v) {
+		dists.push_back(distance(p.x, p.y, x, y));
+	}
+	sort(dists.begin(), dists.end());
+	return (int)dists[n - 1];
+}
---
+++ worley_noise.cpp
@@ -0,0 +1,49 @@
+
+/*https://en.wikipedia.org/wiki/Worley_noise
+  Serkan Can Caglayan 10/9/2020
+*/
+#include "funcs.h"
+int main(int argc, char* argv[]) {
+	if (!SDL_Init(SDL_INIT_VIDEO)){
+		SDL_Window* window;
+		SDL_Renderer* renderer;
+		SDL_Event event;
+		SDL_bool done = SDL_FALSE;
+		vector<SDL_Point> random_points;
+		for (int i = 0; i < number_of_random_points; i++) {
+			int x = rand() % WindowWidth;
+			int y = rand() % WindowHeight;
+			SDL_Point p = { x , y };
+			random_points.push_back(p);
+		}
+		if (!SDL_CreateWindowAndRenderer(WindowWidth, WindowHeight, 0, &window, &renderer)){
+			for (int i = 0; i < WindowWidth; i++) {
+				for (int j = 0; j < WindowHeight; j++) {
+					int color = worley_noise(random_points, i, j, 4);
+					SDL_SetRenderDrawColor(renderer, color, color, color, SDL_ALPHA_OPAQUE);
+					SDL_RenderDrawPoint(renderer, i, j);
+				}
+			}
+			SDL_RenderPresent(renderer);
+			while (!done){
+				SDL_PollEvent(&event);
+				if (event.type == SDL_QUIT) {
+					done = SDL_TRUE;
+				}
+			}
+		}
+		else{
+			std::cerr << "Failed to create window or renderer" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+
+		SDL_DestroyWindow(window);
+		SDL_DestroyRenderer(renderer);
+		SDL_Quit();
+	}
+	else{
+		std::cerr << "Failed to initialize SDL2" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
